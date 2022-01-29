7bb9b370f9001fde9748677fcde60378aa1c650d
thaseus <thaseus@uw.edu>
2022-01-28 23:53:25 -0800

    Sticking point

--- Tank.py
+++ Tank.py
@@ -9,35 +9,49 @@ from matplotlib.pyplot import plot, show
 D0 = 1
 h0 = 2
 d = 0.02
-tSpace = linspace(0, 1900, 10000)
 hf = 0.05
+hSpace = linspace(0, h0, 100)
+
 
 def main():
-    t1 = tanalytic(D0,h0)
-    t2 = tanalytic(1.1 * D0, h0)
-    print(height_diff(1931))
-    print(height_diff(1596))
-    print(brentq(height_diff,height_diff(t1),height_diff(t2)))
-    # show()
+    # Plots parabolic curve to verify that it meets the boundary conditions
+    plot(hSpace, Diameter(hSpace))
+    show()
+    # Creating our bounds using the analytic solution
+    t1 = tanalytic(D0, hf)
+    t2 = tanalytic(1.1 * D0, hf)
+    # Using our functions and bounds to find the drain time (in seconds)
+    print(brentq(height_diff, t1, t2))
+
 
 # Diameter Function
 def Diameter(h):
-    # To get the curve we want we shift our parabola over by h0/2, put our vertex at 1.1D0, and multiply our curve by a constant that equals 0.1 D0 at 0 and h0
-    D = 1.1 * D0 - (0.4 * D0 / h0**2) * (h - h0 / 2)**2
+    # To get the curve we want we shift our parabola over by h0/2, put our vertex at 1.1D0, and multiply our curve by
+    # a constant that equals 0.1 D0 at 0 and h0
+    D = 1.1 * D0 - (0.4 * D0 / h0 ** 2) * (h - h0 / 2) ** 2
     return D
 
-def rate(t,h):
-    dhdt = - sqrt(g / 2) * (d**2 / Diameter(h)**2) * sqrt(h)
+
+# Our differential equation
+def rate(t, h):
+    dhdt = - sqrt(2 * g) * (d ** 2 / Diameter(h) ** 2) * sqrt(h)
     return dhdt
 
+
+# Function for calculating the difference between the height at a time and our final height
 def height_diff(t):
-    diffTup = odeint(rate, h0, [0, t],  tfirst=True)
-    diff = float(diffTup[1])
+    # Uses odeint to solve above diffeq, stores result as a tuple with time and height
+    diffTup = odeint(rate, h0, [0, t], tfirst=True)
+    # Only want the height, so this takes the height value as a float and subtracts the final height
+    diff = float(diffTup[1]) - hf
     return diff
 
-def tanalytic(D,h):
-    t = ((D**2 * sqrt(2)) * (sqrt(h0) + sqrt(h))) / (d**2 * sqrt(g))
+
+# Our analytic solution, assuming no bulge. Used for finding bounds
+def tanalytic(D, h):
+    t = ((D ** 2 * sqrt(2)) * (sqrt(h0) - sqrt(h))) / (d ** 2 * sqrt(g))
     return t
 
 
+# Main function. Runs the whole thing
 main()
